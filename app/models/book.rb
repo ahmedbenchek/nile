081768cf4e9f492c6018a1f65d8ef31743fd2212
ahmedbenchek <ahmedbenchekroun123@gmail.com>
2022-10-27 12:45:06 -0400

    added validations to model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+	attr_accessor :title, :author
+	validates :title, presence: true, length: { minimum:  3 }
+	validates :author, presence: true, length: { minimum:  3 }
+
+
 end
